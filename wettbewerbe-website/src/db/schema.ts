82aa1113c910d64cb411a14e682ed2da5b65c3a5
Starhawk1337 <danielkaiser3001@gmail.com>
2025-01-22 10:36:09 +0100

    changed back primary key to integer generated as identity

--- wettbewerbe-website/src/db/schema.ts
+++ wettbewerbe-website/src/db/schema.ts
@@ -2,7 +2,7 @@ import { integer,serial,smallint,boolean,date, pgTable, varchar,text,primaryKey 
 import {relations } from "drizzle-orm";
 
 export const competition = pgTable("competition", {
-  comp_id: serial().primaryKey(),
+  comp_id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   description: text(),
   lastRegistrationDate: date("date", {mode: "date"}).notNull(),
@@ -12,19 +12,19 @@ export const competition = pgTable("competition", {
 });
 
 export const user = pgTable("user",{
-  user_id: serial().primaryKey(),
+  user_id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar({length: 255}).notNull(),
   password: varchar({length:255}),
   
 });
 
 export const teacher = pgTable("teacher",{
-  user_id: serial().references(()=>user.user_id).primaryKey(),
+  user_id: integer().references(()=>user.user_id).primaryKey(),
   shortName: varchar({length: 10})
 });
 
 export const student = pgTable("student",{
-  user_id: serial().references(()=>user.user_id).primaryKey(),
+  user_id: integer().references(()=>user.user_id).primaryKey(),
   branch: varchar({length:50}),
   class: varchar({length:10}),
 
@@ -33,7 +33,7 @@ export const student = pgTable("student",{
 //RELATIONS
 
 export const interests = pgTable("interests",{
-  int_id: serial().primaryKey(),
+  int_id: integer().primaryKey().generatedAlwaysAsIdentity(),
   user_id: integer().references(()=>user.user_id),
   comp_id: integer().references(()=>competition.comp_id),
   sign_up_date: date("date", {mode: "date"}).notNull(),
